c98d949366513eb06d678a64f10ebdb3c3301939
wasfak <hardmodz@yahoo.com>
2024-01-04 16:15:27 +0200

    finaly

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-
+import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,7 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Toaster position="top-center " />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
--- components/Heading.js
+++ components/Heading.js
@@ -1,6 +1,6 @@
 import { Icon } from "lucide-react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "../lib/utils";
 
 export const Heading = ({
   title,
